59dc8afb5f5c458ab36deaebe2e488a8db0ddd5b
Anmol Sonthalia <61040390+anmol957@users.noreply.github.com>
2021-06-14 12:38:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+// coded by @AAnmol Sonthalia 10th June 2021
+const projectName = 'product-landing-page';
+localStorage.setItem('example_project', 'Product Landing Page');
+// REPLACE CDN WITH GITCDN!!!
